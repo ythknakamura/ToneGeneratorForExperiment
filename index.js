98e7803cbdbe24f9823e8c9cdfa2b6e3a7488646
ythknakamura <ythknakamura@users.noreply.github.com>
2020-11-08 21:20:15 +0900

    Update index.js
    change heltz 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioCtx = null;
 var oscillator;
 var start = false;
-var heltz = 678;
+var heltz = 642;
 
 
 function switchStart(to){
